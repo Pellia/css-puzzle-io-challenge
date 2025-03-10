6ef057998e275e5afb4d0a22b3d06dc7c81a4aa2
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-03-10 21:21:34 +0100

    added navigation base component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Navigation from "./components/Navigation";
 import TextBox from "./components/TextBox";
 
 function App() {
     return (
         <>
             <TextBox />
+            <Navigation />
         </>
     );
 }
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Navigation = () => {
+    return (
+        <div class="navigation-container">
+            <div>Logo</div>
+            <ul>
+                <li>Product</li>
+                <li>Solutions</li>
+                <li>Resources</li>
+                <li>Puzzle vs. Quickbooks</li>
+                <li>Pricing</li>
+            </ul>
+            <div>
+                <p>Login</p>
+                <button>Book a demo</button>
+                <button>Get started for free</button>
+            </div>
+        </div>
+    );
+};
+
+export default Navigation;
