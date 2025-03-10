bd5c6d6c14216caf4479d0bfded4717a33d14ec2
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-03-10 20:47:50 +0100

    added textbox

---
+++ src/components/TextBox.jsx
@@ -0,0 +1,11 @@
+const TextBox = () => {
+    return (
+        <div class="textbox-container">
+            <p class="textbox-text">
+                Why Did Decimal Migrate Hundreds of Clients from KPMG Spark to Puzzle? <a href="">Learn more here</a>
+            </p>
+        </div>
+    );
+};
+
+export default TextBox;
